2d81b0adf58e8bb5bdaa3eca8c277d9abaa6ce37
Milan <pro@turtletv.fr>
2023-04-13 10:11:30 +0200

    🍱 ✨ Add fonts

--- App.tsx
+++ App.tsx
@@ -6,9 +6,33 @@ import { Colors } from "react-native-ui-lib";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
-
+import { useFonts } from "expo-font";
+import {
+  Poppins_100Thin,
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_900Black,
+} from "@expo-google-fonts/poppins";
 export default function App() {
   let persistor = persistStore(store);
+
+  const [loaded] = useFonts({
+    SplatterKings: require("./src/assets/fonts/SplatterKings-Bgex.ttf"),
+    Poppins_100Thin,
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_900Black,
+  });
   return (
     <Provider store={store}>
       <PersistGate
---
+++ src/utils/fonts.utils.ts
@@ -0,0 +1,11 @@
+export enum Fonts {
+  TITLE = "SplatterKings",
+  THIN = "Poppins_100Thin",
+  EXTRA_LIGHT = "Poppins_200ExtraLight",
+  LIGHT = "Poppins_300Light",
+  BODY = "Poppins_400Regular",
+  MEDIUM = "Poppins_500Medium",
+  BOLD = "Poppins_700Bold",
+  EXTRA_BOLD = "Poppins_800ExtraBold",
+  BLACK = "Poppins_900Black",
+}
