fe95ae94282d8c10339187a54c93e96b9985acb8
Milan <pro@turtletv.fr>
2023-04-13 09:21:37 +0200

    ✨ Add template component

---
+++ src/components/common/Template.component.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+
+interface TemplateProps {}
+
+export const Template: React.FunctionComponent<TemplateProps> = ({}) => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
