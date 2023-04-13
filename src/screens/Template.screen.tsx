b9788835123ab1d55002468c507baed683477d2a
Milan <pro@turtletv.fr>
2023-04-13 09:23:34 +0200

    ✨ Add template screen

---
+++ src/screens/Template.screen.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+
+interface TemplateScreenProps {}
+
+export const TemplateScreen: React.FunctionComponent<
+  TemplateScreenProps
+> = ({}) => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
