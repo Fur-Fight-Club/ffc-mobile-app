a2a70f0ec825c22c64023ac112b2512ddaf3cae6
Milan <pro@turtletv.fr>
2023-04-13 09:21:16 +0200

    🔧 Setup aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@environments": "./src/environments",
+            "@navigation": "./src/navigation",
+            "@screens": "./src/screens",
+            "@store": "./src/store",
+            "@utils": "./src/utils",
+          },
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
