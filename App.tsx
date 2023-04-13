2143ab8feab336444638109e6f292a0555ab7687
Milan <pro@turtletv.fr>
2023-04-13 09:24:54 +0200

    ✨ Implement main screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Loader } from "@components/ui/molecules/Loader.component";
+import { PersistGateLoader } from "@components/ui/molecules/PersistGateLoader.component";
+import { store } from "@store/store";
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "react-native-ui-lib";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App() {
+  let persistor = persistStore(store);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <PersistGate
+        persistor={persistor}
+        loading={<PersistGateLoader />}
+      ></PersistGate>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
