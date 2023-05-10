9dbe64a2f0dc08f9820eed3cc22d238f57eae4dc
Milan <pro@turtletv.fr>
2023-05-10 14:42:17 +0200

    ✨ Update test screen

--- src/screens/Test.screen.tsx
+++ src/screens/Test.screen.tsx
@@ -1,42 +1,39 @@
 import { Spacer } from "@components/ui/atoms/Spacer.component";
+import {
+  setToken,
+  useLoginMutation,
+  useTestQuery,
+} from "@store/application/slice";
 import { Fonts, FontsType } from "@utils/fonts.utils";
 import { hp } from "@utils/responsive.utils";
 import * as React from "react";
-import { StyleSheet, Text } from "react-native";
-import { View } from "react-native-ui-lib";
+import { StyleSheet } from "react-native";
+import { TextField, View, Text, Button } from "react-native-ui-lib";
+import {
+  KeyboardTrackingView,
+  KeyboardAwareInsetsView,
+  KeyboardRegistry,
+  KeyboardAccessoryView,
+  KeyboardUtils,
+} from "react-native-ui-lib/keyboard";
+import { useDispatch } from "react-redux";
 
 interface TestScreenProps {}
 
 export const TestScreen: React.FunctionComponent<TestScreenProps> = ({}) => {
+  const dispatch = useDispatch();
+  const [login] = useLoginMutation();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text
-        style={{
-          fontFamily: Fonts.TITLE,
-          fontSize: hp("5%"),
-        }}
-      >
-        Fur Fight Club
-      </Text>
-      <Spacer size={hp("5%")} />
-      {Object.keys(Fonts).map((font) => (
-        <Text
-          key={font}
-          style={{
-            fontFamily: Fonts[font as FontsType],
-            fontSize: hp("3%"),
-            textTransform: "capitalize",
-          }}
-        >
-          {font}
-        </Text>
-      ))}
+    <View flex paddingH-25 paddingT-120>
+      <Button
+        text70
+        white
+        background-orange30
+        label="Logout"
+        onPress={() => dispatch(setToken(""))}
+      />
     </View>
   );
 };
