31944d1f1fb9ba7c59621a89ba9d7ce2b6b89541
Milan <pro@turtletv.fr>
2023-05-11 09:09:46 +0200

    ✨ Implement `Matches` store

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,7 +1,9 @@
 import { applicationApi, applicationSlice } from "./application/slice";
-
+import { matchesApi, matchesSlice } from "./matches/slice";
 
 export const reducers = {
   [applicationApi.reducerPath]: applicationApi.reducer,
   [applicationSlice.name]: applicationSlice.reducer,
+  [matchesApi.reducerPath]: matchesApi.reducer,
+  [matchesSlice.name]: matchesSlice.reducer,
 } as const;
