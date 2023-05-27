ff8d935d349fefd79628c67a2c58d7e0d01b81fb
Milan <pro@turtletv.fr>
2023-05-27 15:17:47 +0200

    🚧 Add native dependencies

--- src/navigation/Main.navigation.tsx
+++ src/navigation/Main.navigation.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { Alert, Platform, StatusBar, StyleSheet, View } from "react-native";
 import { BottomTabNavigation } from "./BottomTab.navigation";
 import { Loader } from "@components/ui/molecules/Loader.component";
 import { useSelector } from "react-redux";
@@ -7,6 +7,8 @@ import { applicationState } from "@store/application/selector";
 import { NotConnectedNavigation } from "./NotConnected.navigation";
 import * as Notifications from "expo-notifications";
 import { useUpsertNotificationTokenMutation } from "@store/application/slice";
+import messaging from "@react-native-firebase/messaging";
+import { useEffect } from "react";
 
 interface MainNavigationProps {}
 
@@ -16,7 +18,19 @@ export const MainNavigation: React.FunctionComponent<
   const { loading, token, notification_token } = useSelector(applicationState);
   const [upsertNotificationToken] = useUpsertNotificationTokenMutation();
 
-  if (!notification_token) {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!");
+      console.log(JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
+
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.log("Message handled in the background!", remoteMessage);
+  });
+
+  if (!notification_token && token !== "") {
     (async () => {
       console.log("No token found, retrieving a notification token...");
       const expoToken = (await Notifications.getDevicePushTokenAsync()).data;
@@ -38,5 +52,16 @@ export const MainNavigation: React.FunctionComponent<
     </>
   );
 };
+
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log("Authorization status:", authStatus);
+  }
+}
 
 const styles = StyleSheet.create({});
